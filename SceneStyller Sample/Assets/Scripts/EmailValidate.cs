aec6582ba0730e5c0c680e00b7f983ef8ba42811
SaiDeekshit <saideeksit@houseofbluebeans.com>
2019-12-02 18:24:23 +0530

    ToolkitPanel
    Description : Toolkit panel done

---
+++ SceneStyller Sample/Assets/Scripts/EmailValidate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class EmailValidate : MonoBehaviour {
+ 
+    public const string MatchEmailPattern =
+        @"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+            + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+            + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+            + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})$";
+
+    public static bool MatchEmail;
+
+    public static bool IsEmail(string email)
+    {
+        if (email != null)
+        {
+            MatchEmail = Regex.IsMatch(email, MatchEmailPattern, RegexOptions.IgnoreCase);
+            return MatchEmail;
+
+        }
+
+        else return false;
+    }
+}
---
+++ SceneStyller Sample/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour
+{
+
+    public GameObject ToolkitCapture_Template;    //Toolkit & Capture Template
+    string CurrentPage = "";                           //CurrentPage (Template)
+
+
+    public GameObject Login_Panel;                //Login & SignUp Panel
+    public GameObject SingUp_Panel;
+
+    public GameObject AccountCreated_Panel;       //After Account Creating Panel
+
+    public Text EmailLogin_Text;                 //Email validation Text
+    public Text EmailSignUp_Text;
+
+    public GameObject GreyOutButtons;            //GreyOut Buttons at Toolkit
+    
+    public GameObject BackButton;
+    public GameObject SaveButton;
+
+    public GameObject Toolkit1_Grey,Toolkit2_Grey,Toolkit3_Grey,Toolkit4_Grey;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToSignUpPanel(){              //for signup button
+    SingUp_Panel.SetActive(true);
+    Login_Panel.SetActive(false);
+    }
+    public void GoToLoginPanel(){              //for login button
+    Login_Panel.SetActive(true);
+    SingUp_Panel.SetActive(false);
+    }
+    public void AccountCreate_Open(){           //for Open Account created Panel
+       if(EmailValidate.IsEmail(EmailSignUp_Text.text)){
+        AccountCreated_Panel.SetActive(true);
+       }
+    }
+    public void AfterLogin(){                   //for loginbutton at login panel
+        if(EmailValidate.IsEmail(EmailLogin_Text.text)){
+        ToolkitCapture_Template.SetActive(true);
+        CurrentPage = "Toolkit";
+        CurrentObjects();
+       }
+    }
+
+    public void Toolkit1(){                         ///Selection of Toolkit
+      CurrentToolkit("one");
+    }
+    public void Toolkit2(){
+      CurrentToolkit("two");
+    }
+    public void Toolkit3(){
+      CurrentToolkit("three");
+    }
+    public void Toolkit4(){
+      CurrentToolkit("four");
+    }
+    void CurrentToolkit(string number){
+        GreyOutButtons.SetActive(false);
+        switch (number)
+        {
+            case "one":
+            Toolkit1_Grey.SetActive(false);
+            Toolkit2_Grey.SetActive(true);
+            Toolkit3_Grey.SetActive(true);
+            Toolkit4_Grey.SetActive(true);
+            break;
+
+            case "two":
+            Toolkit1_Grey.SetActive(true);
+            Toolkit2_Grey.SetActive(false);
+            Toolkit3_Grey.SetActive(true);
+            Toolkit4_Grey.SetActive(true);
+            break;
+
+            case "three":
+            Toolkit1_Grey.SetActive(true);
+            Toolkit2_Grey.SetActive(true);
+            Toolkit3_Grey.SetActive(false);
+            Toolkit4_Grey.SetActive(true);
+            break;
+
+            case "four":
+            Toolkit1_Grey.SetActive(true);
+            Toolkit2_Grey.SetActive(true);
+            Toolkit3_Grey.SetActive(true);
+            Toolkit4_Grey.SetActive(false);
+            break;
+            
+      
+        }
+    }
+
+    public void Deselect_Toolkit(){
+            Toolkit1_Grey.SetActive(true);
+            Toolkit2_Grey.SetActive(true);
+            Toolkit3_Grey.SetActive(true);
+            Toolkit4_Grey.SetActive(true);
+            GreyOutButtons.SetActive(true);
+    }
+
+
+    void CurrentObjects(){
+        switch (CurrentPage)
+        {
+            case "Toolkit":
+            BackButton.SetActive(false);
+            SaveButton.SetActive(false);
+            break;
+            
+            // default:
+        }
+    }
+
+}
